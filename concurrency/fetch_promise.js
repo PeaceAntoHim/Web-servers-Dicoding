941413ed86b461f0eda5b08aa9782add6bb12a2d
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-12-15 00:25:04 +0700

    created new

---
+++ concurrency/fetch_promise.js
@@ -0,0 +1,23 @@
+const fetchingUserFromInternet = (isOffline) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (isOffline) {
+        reject(new NetworkError("Gagal mendapatkan data dari internet"));
+      } else {
+        resolve({ name: "John", age: 18 });
+      }
+    }, 500);
+  });
+};
+
+// TODO: 2
+
+async function gettingUserName() {
+  try {
+    const user = await fetchingUserFromInternet(false);
+
+    return user.name;
+  } catch (error) {
+    return error.message;
+  }
+}
