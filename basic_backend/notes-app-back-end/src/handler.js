f3618aa127f3f1e7e1234bf3b82dabb02dd6863b
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-12-24 23:51:10 +0700

    up

--- basic_backend/notes-app-back-end/src/handler.js
+++ basic_backend/notes-app-back-end/src/handler.js
@@ -1,7 +1,23 @@
+const { nanoid } = require("nanoid");
+const notes = require("./notes");
+
 const addNoteHandler = (request, h) => {
   const { title, tags, body } = request.payload;
 
   const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    updatedAt,
+  };
+
+  notes.push(newNote);
 };
 
 module.exports = { addNoteHandler };
